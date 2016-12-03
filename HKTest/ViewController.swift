0254bd467fd6a9f1504820439655162796c88035
wrl <515045622@qq.com>
2016-12-03 12:11:52 +0800

    Initial Commit

---
+++ HKTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HKTest
+//
+//  Created by 王儒林 on 2016/12/3.
+//  Copyright © 2016年 xingshulin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
